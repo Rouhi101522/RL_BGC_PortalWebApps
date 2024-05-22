849214d7af07e00880c604ea59f049f836b3f4ae
rouh <jgcuhit@up.edu.ph>
2024-05-23 04:11:18 +0800

    FIX BUGS ON HEADER

---
+++ home.php
@@ -0,0 +1,53 @@
+<?php 
+DEFINE("TITLE", "APPLICATION");
+include_once("website/templates/header.php");
+?>
+
+<body>
+    <div class="reqTab">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p>Text</p>
+                </div>
+                <div class="col">
+                    <p>text2</p>
+                </div>
+                <div class="col">
+                    <p>text3</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>Text4</p>
+                </div>
+                <div class="col">
+                    <p>Text5</p>
+                </div>
+                <div class="col">
+                    <p>Text6</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>Text7</p>
+                </div>
+                <div class="col">
+                    <p>Text8</p>
+                </div>
+                <div class="col">
+                    <p>Text9</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>Text4</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php 
+include_once("website/templates/footer.php");
+?>
